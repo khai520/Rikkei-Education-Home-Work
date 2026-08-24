1c1c8dc0ce51f36912fbdb23f7033f523ccad36c
khai520 <45537943+khai520@users.noreply.github.com>
2026-08-24 11:04:05 +0700

    update

---
+++ DuAn/JavaBE/QLTT/src/test/java/org/example/qltt/QlttApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qltt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
